a2c63b65633d6f742c11fdb15f8873d1c36d9f3f
kalimagham <kalimagham@deloitte.pt>
2021-05-25 16:48:57 +0100

    Removed single quotes from uri.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if uri.startswith("postgres://"):
 # rest of connection code using the connection string `uri`
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('uri', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
